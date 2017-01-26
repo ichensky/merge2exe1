d21a26c39cad82741c07a8511fa7b0bb0d719402
ichensky <ichensky@live.com>
2017-01-26 11:15:56 +0200

    make refactoring of build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
-gcc evil.c -o evil.exe
-gcc real.c -o real.exe
-#
-echo "const unsigned char evil[] = {" > evil.h
-cat evil.exe | hexdump -v -e '"0x" 1/1 "%02X" ","' | sed s/,$// >> evil.h
-echo "};" >> evil.h
-#
-#
-echo "const unsigned char real[] = {" > real.h
-cat real.exe | hexdump -v -e '"0x" 1/1 "%02X" ","' | sed s/,$// >> real.h
-echo "};" >> real.h
-#
-gcc main.c -o a.exe
+#!/bin/sh
+
+function bin_to_hex {
+	echo "const unsigned char $1[] = {" > $1.h
+	cat $1.exe | hexdump -v -e '"0x" 1/1 "%02X" ","' | sed s/,$// >> $1.h
+	echo "};" >> $1.h
+}
+
+evil=evil
+real=real
+app=a
+
+gcc $evil.c -o $evil.exe
+gcc $real.c -o $real.exe
+
+bin_to_hex $evil
+bin_to_hex $real
+
+gcc main.c -o $app.exe
